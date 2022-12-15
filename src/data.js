d98b88f42bd4af0460ce71f85b6cbb4371677819
Brandon Seymour <bseymour122@yahoo.ca>
2022-12-15 13:23:18 -0500

    Add education form fields to data.js

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const education = [
+  {
+    type: 'text',
+    id: 'school',
+    name: 'school',
+    labelText: 'School',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'degree',
+    name: 'degree',
+    labelText: 'Degree',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'start',
+    name: 'start',
+    labelText: 'Start Date',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'end',
+    name: 'end',
+    labelText: 'End Date',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'location',
+    name: 'location',
+    labelText: 'Location',
+    value: '',
+  },
+];
+
+export { education };
