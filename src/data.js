8e7ec2f59fa99ec1a3bdded571ac9d5303db310b
Brandon Seymour <bseymour122@yahoo.ca>
2023-01-16 13:54:14 -0500

    Add experience form fields to data.js

--- src/data.js
+++ src/data.js
@@ -36,4 +36,49 @@ const education = [
   },
 ];
 
-export { education };
+const experience = [
+  {
+    type: 'text',
+    id: 'company',
+    name: 'company',
+    labelText: 'Company Name',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'position',
+    name: 'position',
+    labelText: 'Position Title',
+    value: '',
+  },
+  {
+    type: 'textarea',
+    id: 'description',
+    name: 'description',
+    labelText: 'Job Description',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'start',
+    name: 'start',
+    labelText: 'Start Date',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'end',
+    name: 'end',
+    labelText: 'End Date',
+    value: '',
+  },
+  {
+    type: 'text',
+    id: 'location',
+    name: 'location',
+    labelText: 'Location',
+    value: '',
+  },
+];
+
+export { education, experience };
